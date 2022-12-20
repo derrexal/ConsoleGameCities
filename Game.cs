e0ed3ce994e50e627224c7aeb646ef875d17591b
derrexal <derrexal@gmail.com>
2022-12-20 20:05:53 +0300

    Вечерний сейв

--- Game.cs
+++ Game.cs
@@ -3,10 +3,11 @@
  * Программа основанная на игре "Города"
  * Вычисляет саммую длинную партию, которую теоретически можно сыграть.
  * Идеи:
- * 1 - выводить процент использования городов
- * 2 - улучшить отбор следующего города, н-р выбирать так, чтобы следующие ходы потенциально не уменьшали запасы слов на редко используемые буквы.
+ * Предусмотреть случай, когда последние символы города - не буквы? (Алкино-2)
+ * ! Что можем сделать с последними?
  */
 
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Games_Cities
@@ -17,41 +18,31 @@ namespace Games_Cities
         static List<string> cities = new List<string>();
         static List<string> cities_copy = new List<string>();
         static List<string> longest_part_game = new List<string>(); // буферный список для сохранения текущей партии
-        static var percent_max;
+        static float percent_max;
+        static int default_list_size;
+        static Dictionary<char,int> alphabet_weight = new Dictionary <char,int>(); // словарь, содержащий все буквы, на которые начинаются города и их веса
+
         // Главная функция
         static void Main(string[] args)
         {
             int i = 0;
             
             Console.WriteLine("Ну что, поиграем?");
-            fill_CitiesArray(cities_list_path);//заполняем список городами из txt
+            fill_cities_array(cities_list_path);//заполняем список городами из txt
             Console.WriteLine("Общее количество городов в игре: " + cities.Count);
-            while(i!=1000)
+            calculation_the_weight_of_the_alphabet();
+            while (i!=100) //Никакого рандома, кроме выбора первой жертвы нет.
             {
                 determine_the_longest_batch(); // непосредственно процесс
                 cities = new List<string>(); // обнуляем список с которым работали
                 cities.AddRange(cities_copy); // и заполняем его снова всеми городами из txt 
                 i = i + 1;
             }
-            Console.WriteLine("Длинна max: " + longest_part_game.Count());
-            Console.WriteLine("Процент использования городов: " + percent_max);
+            Console.WriteLine($"Максимальная длинна партии: {longest_part_game.Count()} \nПроцент использования словаря: {percent_max:0.00}");
             //Console.WriteLine(string.Join("\t", longest_part_game));
-
         }
 
-        // Читает файл с городами и записывает все их в список
-        static void fill_CitiesArray(string filepath)
-        {
-            using (StreamReader reader = new StreamReader(filepath)) //убрал async 
-            {
-                string? line; // переменная может принимать значение null
-                while ((line = reader.ReadLine()) != null)
-                {
-                    cities.Add(line.ToLower());
-                }
-            }
-            cities_copy.AddRange(cities);
-        }
+        
         // определение самой длинной партии
         static int determine_the_longest_batch()
         {
@@ -73,7 +64,7 @@ namespace Games_Cities
             {
                 longest_part_game = new List<string>();
                 longest_part_game.AddRange(buffer_of_matches);
-                percent_max = cities.Count / cities_copy.Count;
+                percent_max = (float) buffer_of_matches.Count / cities_copy.Count*100;
             } 
             return buffer_of_matches.Count;
         }
@@ -81,9 +72,8 @@ namespace Games_Cities
         // Возвращает город из списка, на основании последней буквы аргумента
         static string? find_next_city(string city)
         {
-            //Console.WriteLine(city + "  ");
             char last_sym = last_symbol(city);
-            string? n_city = find_val(last_sym);    //string? n_city = cities.Find(n_city => n_city.StartsWith(last_sym));     // !!!!! подряд перебирает все элементы списка и ищет следующий город. Нужно оптимизировать...
+            string? n_city = Find_val(last_sym);    //string? n_city = cities.Find(n_city => n_city.StartsWith(last_sym));     // !!!!! подряд перебирает все элементы списка и ищет следующий город. Нужно оптимизировать...
             if (n_city != null) //Если город найден
             {
                 cities.Remove(n_city); // Удаляем из списка
@@ -91,25 +81,39 @@ namespace Games_Cities
             }
             else return null;
         }
+
         // ищет все города из списка на букву "*" и выбирает из всех рандомный
-        static string? find_val(char last_sym)
+        // совершает одно действие - отдает город из списка
+        static string? Find_val(char last_sym)
         {
-            Random random = new Random();
-            List<string> cities_starting_with_a_letter = new List<string>();
+            List<string> cities_starting_with_a_letter = new List<string>(); // в этот список кладет все города на букву "*". Почему бы не сохранить его и не использовать в будущем?
             foreach (string city in cities)
             {
                 if (Char.Parse(city.Substring(0, 1)) == last_sym)
                 {
-                    cities_starting_with_a_letter.Add(city);
+                    cities_starting_with_a_letter.Add(city); // формируем список городов, начинающихся на "*"
                 }
             }
-            if (cities_starting_with_a_letter.Count > 0)
+            if (cities_starting_with_a_letter.Count == 1) return cities_starting_with_a_letter[0];
+            if (cities_starting_with_a_letter.Count > 0) // если в массиве что-то есть
             {
-                int index = random.Next(0, cities_starting_with_a_letter.Count);
-                return cities_starting_with_a_letter[index];
+                foreach (var letter in alphabet_weight) // логика в том, чтобы последняя буква возвращаемого слова оканчивалась на самую распространенную в данный момент.
+                {
+                    foreach (string city in cities)
+                    {
+                        if (Char.Parse(city.Substring(city.Length - 1, 1)) == letter.Key)
+                        {
+                            alphabet_weight[letter.Key] = alphabet_weight[letter.Key] - 1;
+                            return city;
+                        }
+                    }
+                }
             }
             else return null;
+
+            return null; // компилятор без неё ругается, но мне видится что сюда никогда не зайдет.
         }
+
 
         // определяет последний символ города
         static char last_symbol(string city,int step=1)
@@ -122,7 +126,42 @@ namespace Games_Cities
             }
             if (last_sym == 'й') last_sym = 'и'; // а как сделать так, чтобы в первый раз он брал "йошкар-ола" а затем менял на "И"
             return last_sym;
-        }   
+        }
+
+        // определяет вес каждой буквы-начала городов из списка и заполняет соответствуюший словарь
+        static void calculation_the_weight_of_the_alphabet()
+        {
+            foreach(string city in cities)
+            {
+                int default_weight = 1;
+                char first_symbol = Char.Parse(city.Substring(0, 1)); //Определяем первую букву
+                if (alphabet_weight.ContainsKey(first_symbol) == false) // Если её ещё нет в словаре - добавляем и присваиваем вес default_weight
+                {
+                    alphabet_weight.Add(first_symbol, default_weight);
+                }
+                else // А если есть - увеличиваем вес на default_weight
+                {
+                    alphabet_weight[first_symbol]= alphabet_weight[first_symbol]+default_weight;
+                }
+            }
+            alphabet_weight = alphabet_weight.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        // Читает файл с городами и записывает все их в список
+        static void fill_cities_array(string filepath)
+        {
+            using (StreamReader reader = new StreamReader(filepath)) //убрал async - программа не успевала посчитать, а уже выводила результат. КОроче тут ни к чему.
+            {
+                string? line; // переменная может принимать значение null
+                while ((line = reader.ReadLine()) != null)
+                {
+                    cities.Add(line.ToLower());
+                }
+            }
+            cities_copy.AddRange(cities); // Сразу делаем копию исходного списка
+            default_list_size = cities.Count; // и записываем в переменную его размер
+        }
+
     }
 }
 

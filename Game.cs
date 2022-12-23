90bb24321313cc20ae30094ffbbc92f70b4cd972
derrexal <derrexal@gmail.com>
2022-12-23 14:31:19 +0300

    Значительно изменил код. 23.12

--- Game.cs
+++ Game.cs
@@ -10,8 +10,6 @@
 *Идеи:
  * Предусмотреть случай, когда последние символы города - не буквы? (Алкино-2)
  * ! Что можем сделать с последними?
- * Копия списка больше не нужна?
- * Рандом больше не нужен? Первое слово в цепочке - слово, начинающееся на редкую букву, и оканчивающееся на самую частую.
  * 117 строчка!
  */
 
@@ -19,6 +17,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 
 namespace Games_Cities
 {
@@ -26,57 +25,45 @@ namespace Games_Cities
     {
         static string cities_list_path = "C:\\Users\\Ermol\\source\\repos\\Games_Cities\\Cities_Ru.txt";
         static List<string> cities = new List<string>();
-        static List<string> cities_copy = new List<string>();
         static List<string> longest_part_game = new List<string>(); // буферный список для сохранения текущей партии
-        static float percent_max;
-        static int default_list_size;
+        static float percent_max; // Процент использования словаря
+        static int default_list_size; // Общее количество городов в игре
         static Dictionary<char,int> alphabet_weight = new Dictionary <char,int>(); // словарь, содержащий все буквы, на которые начинаются города и их веса
 
         // Главная функция
         static void Main(string[] args)
         {
-            int i = 0;
-            
             Console.WriteLine("Ну что, поиграем?");
             fill_cities_array(cities_list_path);//заполняем список городами из txt
-            Console.WriteLine("Общее количество городов в игре: " + cities.Count);
-            calculation_the_weight_of_the_alphabet();
-            while (i!=100) //Никакого рандома, кроме выбора первой жертвы нет.
-            {
-                determine_the_longest_batch(); // непосредственно процесс
-                cities = new List<string>(); // обнуляем список с которым работали
-                cities.AddRange(cities_copy); // и заполняем его снова всеми городами из txt 
-                i = i + 1;
-            }
+            Console.WriteLine("Общее количество городов в игре: " + default_list_size);
+            calculation_the_weight_of_the_alphabet(); // определяет вес каждой первой буквы города
+            
+            determine_the_longest_batch(); // непосредственно процесс
+            
             Console.WriteLine($"Максимальная длинна партии: {longest_part_game.Count()} \nПроцент использования словаря: {percent_max:0.00}");
-            //Console.WriteLine(string.Join("\t", longest_part_game));
+            //Console.WriteLine(string.Join("\t", longest_part_game)); // выводит всю цепочку
         }
 
-        
         // определение самой длинной партии
-        static int determine_the_longest_batch()
+        static void determine_the_longest_batch()
         {
-            Random random = new Random();
-            int index = random.Next(0, cities.Count);
             string? last_city;
-            List <string> buffer_of_matches = new List<string>();
-            last_city = find_next_city(cities[index]);   //случайным образом выбираем первую жертву !!! 
+            string first_city; //первый город в цепочке
+            char the_rarest_letter_in_the_dictionary = alphabet_weight.Keys.Last();
+            first_city = Find_val(the_rarest_letter_in_the_dictionary); // параметр - самая нераспрастраненная буква.
+            alphabet_weight[the_rarest_letter_in_the_dictionary] = alphabet_weight[the_rarest_letter_in_the_dictionary] - 1; // у этой буквы уменьшаем вес. 
+            if (alphabet_weight[the_rarest_letter_in_the_dictionary] == 0) alphabet_weight.Remove(the_rarest_letter_in_the_dictionary);
+            last_city = find_next_city(first_city);   //Сюда нужно всего лишь передать город, который начинается на самую нераспрастраненную первую букву
 
             while (last_city != null) // пока цепочка не закончится. Здесь происходит непосредственно процесс
             {
                 last_city = find_next_city(last_city);
                 if (last_city != null)
                 {
-                    buffer_of_matches.Add(last_city);
+                    longest_part_game.Add(last_city);
                 }
             }
-            if (buffer_of_matches.Count > longest_part_game.Count)
-            {
-                longest_part_game = new List<string>();
-                longest_part_game.AddRange(buffer_of_matches);
-                percent_max = (float) buffer_of_matches.Count / cities_copy.Count*100;
-            } 
-            return buffer_of_matches.Count;
+            percent_max = (float)longest_part_game.Count / default_list_size * 100; 
         }
 
         // Возвращает город из списка, на основании последней буквы аргумента
@@ -87,46 +74,13 @@ namespace Games_Cities
             if (n_city != null) //Если город найден
             {
                 cities.Remove(n_city); // Удаляем из списка
-                return n_city;  // Вернем город
+                return n_city;  // И возвращаем его
             }
-            else return null;
+            else return null; // если город не найден - вернем нул - игра окончена
         }
-
-        // ищет все города из списка на букву "*" и выбирает из всех рандомный
-        // совершает одно действие - отдает город из списка
-        static string? Find_val(char last_sym)
-        {
-            List<string> cities_starting_with_a_letter = new List<string>(); // в этот список кладет все города на букву "*". Почему бы не сохранить его и не использовать в будущем?
-            foreach (string city in cities)
-            {
-                if (Char.Parse(city.Substring(0, 1)) == last_sym)
-                {
-                    cities_starting_with_a_letter.Add(city); // формируем список городов, начинающихся на "*"
-                }
-            }
-            if (cities_starting_with_a_letter.Count == 1) return cities_starting_with_a_letter[0];
-            if (cities_starting_with_a_letter.Count > 0) // если в массиве что-то есть
-            {
-                foreach (var letter in alphabet_weight) // логика в том, чтобы последняя буква возвращаемого слова оканчивалась на самую распространенную в данный момент.
-                {
-                    foreach (string city in cities)
-                    {
-                        if (Char.Parse(city.Substring(city.Length - 1, 1)) == letter.Key)
-                        {
-                            alphabet_weight[letter.Key] = alphabet_weight[letter.Key] - 1;
-                            return city;
-                        }
-                    }
-                }
-            }
-            else return null;
-
-            return null; // компилятор без неё ругается, но мне видится что сюда никогда не зайдет.
-        }
-
 
         // определяет последний символ города
-        static char last_symbol(string city,int step=1)
+        static char last_symbol(string city, int step = 1)
         {
             char last_sym = Char.Parse(city.Substring(city.Length - step, 1));
             if (last_sym == 'ь' || last_sym == 'ъ' || last_sym == 'ы')// Если последний символ города - ъьы - возвращаем предпоследний и т.д.
@@ -137,6 +91,47 @@ namespace Games_Cities
             //if (last_sym == 'й') last_sym = 'и'; // а как сделать так, чтобы в первый раз он брал "йошкар-ола" а затем менял на "И"
             return last_sym;
         }
+
+
+        // Возвращает город, который заканчивается на самую распространенную букву
+        static string? Find_val(char last_sym)
+        {
+            List<string> cities_starting_with_a_specific_letter = create_list_of_cities_with_a_specific_letter(last_sym); // в этот список кладет все города на букву "*". Почему бы не сохранить его и не использовать в будущем?
+            
+            if (cities_starting_with_a_specific_letter.Count > 0) // если в массиве что-то есть
+            {
+                if (cities_starting_with_a_specific_letter.Count == 1) return cities_starting_with_a_specific_letter[0]; // если по итогу в списке оказался 1 город
+                foreach (var letter in alphabet_weight.Keys) // спмсок самых распространенных первых букв городов по убыванию
+                {
+                    foreach (string city in cities_starting_with_a_specific_letter)
+                    {
+                        if (Char.Parse(city.Substring(city.Length - 1, 1)) == letter) // Если последняя буква слова оканчивается на самую распространенную в данный момент.
+                        {
+                            alphabet_weight[letter] = alphabet_weight[letter] - 1; // у этой буквы уменьшаем вес. 
+                            if (alphabet_weight[letter] == 0) alphabet_weight.Remove(letter); // удаляет элемент из словаря, если он равен 0
+                            return city;
+                        }
+                    }
+                }
+            }
+            else return null;
+
+            return null; // компилятор без неё ругается, но мне видится что сюда никогда не зайдет.
+        }
+        // ищет и возвращает списком все города на букву *
+        static List<string> create_list_of_cities_with_a_specific_letter(char last_sym)
+        {
+            List<string> cities_starting_with_a_specific_letter = new List<string>(); // в этот список кладет все города на букву "*". Почему бы не сохранить его и не использовать в будущем?
+            foreach (string city in cities) // перебираем все города
+            {
+                if (Char.Parse(city.Substring(0, 1)) == last_sym) //если первая буква города = *
+                {
+                    cities_starting_with_a_specific_letter.Add(city); // формируем список городов, начинающихся на "*"
+                }
+            }
+            return cities_starting_with_a_specific_letter;
+        }
+
 
         // определяет вес каждой буквы-начала городов из списка и заполняет соответствуюший словарь
         static void calculation_the_weight_of_the_alphabet()
@@ -168,9 +163,7 @@ namespace Games_Cities
                     cities.Add(line.ToLower());
                 }
             }
-            cities_copy.AddRange(cities); // Сразу делаем копию исходного списка
-            default_list_size = cities.Count; // и записываем в переменную его размер
+            default_list_size = cities.Count; // Количество всех городов
         }
-
     }
 }
